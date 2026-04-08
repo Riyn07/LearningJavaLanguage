023c4608624b67c3167e80f57d1742cba473fca3
vrma <vrmachado@gmail.com>
2026-04-08 14:58:08 +0200

    modificadores de acceso inicialmente explicados

--- LearningJavaLanguage/src/main/java/com/example/App.java
+++ LearningJavaLanguage/src/main/java/com/example/App.java
@@ -4,6 +4,12 @@ public class App {
 	
 	public static void main(String[] args) {
 		
+		Persona persona1 = new Persona();
+		
+		persona1.setNombre("Dani");
+		
+		System.out.println("El valor de la variable "
+				+ "nombre para la referencia persona1 es: " + persona1.getNombre());
 		
 	}
 }
--- LearningJavaLanguage/src/main/java/com/example/Persona.java
+++ LearningJavaLanguage/src/main/java/com/example/Persona.java
@@ -1,12 +1,57 @@
 package com.example;
 
 public class Persona {
-	
+
 	/*
-	 * A continuacion las variables miembros de la clase, que se
-	 * le llaman: propiedades, campos y atributos */
-	
- 	String nombre;
- 	String primerApellido;
- 	String segundoApellido;
+	 * A continuacion las variables miembros de la clase, que se le llaman:
+	 * propiedades, campos y atributos
+	 */
+
+	/*
+	 * Modificadores de acceso:
+	 * 
+	 * 1- privado del paquete, es el por defecto 2- public => es accesible en todos
+	 * los paquetes 3- private => es solo accesible por elementos de la misma clase,
+	 * dentro de la propia clase, incluso un objeto o una instancia de la misma
+	 * clase no puede acceder a las propiedades privadas 4- protected => herencia,
+	 * que veremos posteriormente
+	 * 
+	 * IMPORTANTE !!! Una clase bien diseñada es aquella que tiene todos sus
+	 * propiedades, o campos, privados y solamente se relaciona con el mundo
+	 * exterior a traves de metodos publicos que acceden a dichas variables privadas
+	 */
+
+	private String nombre;
+	private String primerApellido;
+	private String segundoApellido;
+
+	/*
+	 * Metodos Getter and Setter, son metodos publicos a traves de los cuales se
+	 * pueden modificar las variable privadadas de la clase
+	 */
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+
 }

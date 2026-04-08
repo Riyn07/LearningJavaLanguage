5b31d746ef850c6cd340cad2bd2569994a866909
vrma <vrmachado@gmail.com>
2026-04-08 14:26:51 +0200

    eliminado el contenido del metodo main

--- LearningJavaLanguage/src/main/java/com/example/App.java
+++ LearningJavaLanguage/src/main/java/com/example/App.java
@@ -4,26 +4,6 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-		// Creacion de objetos Persona, a partir de la clase Persona
-		
-		Persona persona1 = new Persona();
-		
-		persona1.nombre = "Dani";
-		
-		Persona persona2 = new Persona();
-		
-		persona2.nombre = "Dani";
-		
-		new Persona().nombre = "Andres";
-		
-		/* persona1 y persona2 son iguales ???? */
-		
-		if (persona1 == persona2) 
-			System.out.println("persona1 y persona2 apuntan al mismo objeto");
-		else 
-			System.out.println("persona1 y persona2 son variables de referencia "
-					+ "que apuntan a objetos diferentes en la memoria RAM");
-		
 		
 	}
 }

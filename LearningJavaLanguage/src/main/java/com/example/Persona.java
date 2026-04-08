2c21543a8b33f65071aedb20a427b92007a3073b
vrma <vrmachado@gmail.com>
2026-04-08 14:18:22 +0200

    antes de estudiar los modificadores de acceso

--- LearningJavaLanguage/src/main/java/com/example/Persona.java
+++ LearningJavaLanguage/src/main/java/com/example/Persona.java
@@ -5,4 +5,8 @@ public class Persona {
 	/*
 	 * A continuacion las variables miembros de la clase, que se
 	 * le llaman: propiedades, campos y atributos */
+	
+ 	String nombre;
+ 	String primerApellido;
+ 	String segundoApellido;
 }

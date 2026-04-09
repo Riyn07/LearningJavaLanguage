d7580a4ecaf80aee562f32f0a9c57afe3e484fc8
vrma <vrmachado@gmail.com>
2026-04-09 11:34:28 +0200

    Antes de crear rama para utilizar Lombok

--- LearningJavaLanguage/src/main/java/com/example/Persona.java
+++ LearningJavaLanguage/src/main/java/com/example/Persona.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class Persona {
+public class Persona extends Object {
 
 	/*
 	 * A continuacion las variables miembros de la clase, que se le llaman:
@@ -24,15 +24,72 @@ public class Persona {
 	private String nombre;
 	private String primerApellido;
 	private String segundoApellido;
+	private double salario;
+	
 
+
+	/* Constructores:
+	 * 
+	 * ¿Que es un constructor? Es un metodo (funcion que esta declarada dentro de la clase)
+	 * que tiene el mismo nombre de la clase, y que se utiliza conjuntamente con el operador
+	 * new para construir, instanciar, objetos.
+	 * 
+	 * Por defecto, si no te creas ningun otro constructor, la clase tiene disponible el 
+	 * llamado constructor sin parametros. 
+	 * 
+	 * ¿Como es capaz dicho constructor sin parametros de construir un objeto si en el cuerpo
+	 * de dicho constructor no hay nada de codigo?
+	 * 
+	 * La Respuesta es: Que todas las clases heredan de la clase Object, que es la madre
+	 * de todas las clases, y por tanto el constructor sin parametros lo que hace es invocar
+	 * al constructor de la clase Object para que le haga los deberes, es decir, le construya
+	 * el objeto, que la propia clase no tiene ni idea que de como hacerlo */
+
+	public Persona(String nombre, String primerApellido, String segundoApellido) {
+		super();
+		this.nombre = nombre;
+		this.primerApellido = primerApellido;
+		this.segundoApellido = segundoApellido;
+	}
+
+	
+	public Persona(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	
+	
+	public Persona(double salario) {
+		super();
+		this.salario = salario;
+	}
+
+
+	public Persona(String nombre, String primerApellido) {
+		super();
+		this.nombre = nombre;
+		this.primerApellido = primerApellido;
+	}
+
+	public Persona() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 	/*
 	 * Metodos Getter and Setter, son metodos publicos a traves de los cuales se
 	 * pueden modificar las variable privadadas de la clase
 	 */
 
+
+
+
 	public String getNombre() {
 		return nombre;
 	}
+
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
@@ -54,4 +111,13 @@ public class Persona {
 		this.segundoApellido = segundoApellido;
 	}
 
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", primerApellido=" + primerApellido + ", segundoApellido="
+				+ segundoApellido + "]";
+	}
+
+	
+	
 }
---
+++ LearningJavaLanguage/src/main/java/com/example2/TestPrivadoPaquete.java
@@ -0,0 +1,8 @@
+package com.example2;
+
+import com.example.Persona;
+
+public class TestPrivadoPaquete {
+
+	// Persona p = new Persona(nombre, primerApellido, segundoApellido)
+}

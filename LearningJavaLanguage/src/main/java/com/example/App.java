ecc9bcee7b8fb91bd3cdc034840f0b5cac70dc0e
vrma <vrmachado@gmail.com>
2026-04-09 14:36:06 +0200

    antes de explicar el modificador static

--- LearningJavaLanguage/src/main/java/com/example/App.java
+++ LearningJavaLanguage/src/main/java/com/example/App.java
@@ -1,9 +1,39 @@
 package com.example;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class App {
 	
 	public static void main(String[] args) {
 		
+		/* Creacion o instanciacion de objetos utilizando el patron Builder de Lombok
+		 * , que no fue creado por Lombok pero si lo han mejorado para su utilizacion.
+		 * 
+		 * El patron Builder es llamado un patron de diseño Factory, es decir, es un patron
+		 * de diseño creacional (para la creacion de objetos). 
+		 * 
+		 * ¿Que ventajas nos aporta?
+		 * 
+		 * Nos permite crear objetos aunque no tengamos el constructor explicitamente 
+		 * implementado, y ademas cuando vamos a crear un objeto el orden en que utilicemos
+		 * las propiedades o campos de la clase es indiferente */
+		
+		Persona p1 = Persona.builder()
+				.salario(3500.25)
+				.nombre("Carolina")
+				.build();
+		
+		Persona persona2 = Persona.builder()
+				.nombre("Sebastian")
+				.primerApellido("Sanjuanelo")
+				.segundoApellido("Arrieta")
+				.genero(Genero.HOMBRE)
+				.fechaNacimiento(LocalDate.of(2000, Month.JANUARY, 20))
+				.salario(4500.25)
+				.build();
+		
+		System.out.println(persona2);
 
 		
 	}

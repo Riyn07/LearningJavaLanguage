8faf01a58ddc954dbbd97e10c47a562f683dfb8b
vrma <vrmachado@gmail.com>
2026-04-09 14:12:16 +0200

    Antes de utilizar el patron Builder de Lombok

--- LearningJavaLanguage/src/main/java/com/example/Persona.java
+++ LearningJavaLanguage/src/main/java/com/example/Persona.java
@@ -1,11 +1,24 @@
 package com.example;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Persona  {
 
 
 	private String nombre;
 	private String primerApellido;
 	private String segundoApellido;
+	private Genero genero;
+	private LocalDate fechaNacimiento;
 	private double salario;
 	
 	

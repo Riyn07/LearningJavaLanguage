55fc6c9e115df9311bd87db39e577b784a44c3f1
vrma <vrmachado@gmail.com>
2026-04-13 10:39:59 +0200

    Antes de ver el tipo Arrays

--- LearningJavaLanguage/src/main/java/com/example/App.java
+++ LearningJavaLanguage/src/main/java/com/example/App.java
@@ -1,49 +1,61 @@
 package com.example;
 
-import java.time.LocalDate;
-import java.time.Month;
-
 public class App {
+	
+	// Declaracion de variables de instancia
+	// A nombre se le asignara el valor null y a total el valor cero
+	private String nombre;
+	private int total;
 	
 	public static void main(String[] args) {
 		
-		// Ya es posible crear instancias de Empleado
-		Empleado empleado1 = new Empleado();
+		/* VARIABLES. 
+		 * 
+		 * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html 
+		 * 
+		 * Para nombrar una variable hay que tener en cuenta lo siguiente: 
+		 * 
+		 * 1- El primer caracter no puede ser un numero.
+		 * 2- Y aunque se permiten los caracteres $ y underscore (_) tampoco deben
+		 * de utilizarse como el primer caracter de un identificador, es decir, del 
+		 * nombre de una variable
+		 * 
+		 *  Las variables que se declararan a nivel de la clase, no es necesario 
+		 *  asignarles ningun valor por defecto, porque se le asigna un valor por defecto
+		 *  automaticamente. Todo lo contrario ocurre con las variable locales. 
+		 *  Las varibles locales son aquellas que se declaran detro de un bloque, es decir,
+		 *  entre llaves de apertura y cierre {}.
+		 *  Aunque podemos declarar variables dentro de cualquier bloque {}, cuando nos
+		 *  referimos a variables locales significa dentro de un metodo */
 		
-		empleado1.setNombre("Jeronimo");
+		int total2 = 0 ;
+		
+		/* TIPOS DE DATO:
+		 * 
+		 * 1- Primitivos (el nombre es todo en minisculas)
+		 * 2- Objetos o Referencia (La primera letra del tipo es en mayusculas)
+		 * 
+		 * El tipo de dato que realmente existe es el tipo de dato primitivo, que esta
+		 * integrado en el propio lenguaje Java, porque los tipos Objetos o Referencia son 
+		 * un elvoltorio de los tipos primitivos, en resumen, que la gran mayoria de las
+		 * ocasiones debemos utilizar tipos de datos primitivos en lugar de su 
+		 * correspondiente tipo Objeto o Referencia. 
+		 * 
+		 * Los tipos de datos primitivos no tienen propiedades ni metodos, es decir, que 
+		 * si le agregamos el operador punto (.) no se va a desplegar ningun menu
+		 * contextual */
+		
+		Integer total3 = 24;
 		
 		
-		Empleado empleado2 = Empleado.builder()
-				.salario(3500.45)
-				.dpto(Dpto.RRHH)
-				.fechaAlta(LocalDate.of(2000, Month.SEPTEMBER, 1))
-				.ssn("25678954")
-				.build();
-		
-		System.out.println(empleado2.toString());
-		
-		/*
-		 * Crear un empleado que tenga todo lo de una Persona y ademas lo propio del 
-		 * empleado, teniendo en cuenta que los constructores no se heredan 
-		 * */
-		
-//		Empleado empleado3 = new Empleado("Juan", "Mtnez", "Lopez", 
-//				Genero.HOMBRE, LocalDate.of(1980, Month.JANUARY, 20), 
-//				5000, Dpto.FINANZAS, LocalDate.of(2000, Month.SEPTEMBER, 12));
-		
-		Empleado empleado3 = Empleado.builder()
-				.nombre("Juan")
-				.primerApellido("Mtnez")
-				.segundoApellido("Lopez")
-				.genero(Genero.HOMBRE)
-				.fechaNacimiento(LocalDate.of(1980, Month.JANUARY, 20))
-				.salario(5000.24)
-				.dpto(Dpto.FINANZAS)
-				.fechaAlta(LocalDate.of(2000, Month.SEPTEMBER, 12))
-				.ssn("28987654326")
-				.build();
-		
-		System.out.println(empleado3);
-
 	}
 }
+
+
+
+
+
+
+
+
+

23e3d1e110332ed31a229e6c5e96eebd498e7cb5
vrma <vrmachado@gmail.com>
2026-04-13 10:58:39 +0200

    comentario agregado a la clase persona

--- LearningJavaLanguage/src/main/java/com/example/Persona.java
+++ LearningJavaLanguage/src/main/java/com/example/Persona.java
@@ -2,7 +2,9 @@ package com.example;
 
 public class Persona  {
 
-
+	// Modificacion realizada en un commit
+	// para lo cual ha sido necesario la creacion de un rama
+	
 	private String nombre;
 	private String primerApellido;
 	private String segundoApellido;

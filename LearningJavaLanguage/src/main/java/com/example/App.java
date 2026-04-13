fa8bc4ce3307f837a9e817fad09a97911d9f09d4
vrma <vrmachado@gmail.com>
2026-04-13 10:44:06 +0200

    Inicio del trabajo con los Arrays

--- LearningJavaLanguage/src/main/java/com/example/App.java
+++ LearningJavaLanguage/src/main/java/com/example/App.java
@@ -2,52 +2,9 @@ package com.example;
 
 public class App {
 	
-	// Declaracion de variables de instancia
-	// A nombre se le asignara el valor null y a total el valor cero
-	private String nombre;
-	private int total;
-	
 	public static void main(String[] args) {
 		
-		/* VARIABLES. 
-		 * 
-		 * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html 
-		 * 
-		 * Para nombrar una variable hay que tener en cuenta lo siguiente: 
-		 * 
-		 * 1- El primer caracter no puede ser un numero.
-		 * 2- Y aunque se permiten los caracteres $ y underscore (_) tampoco deben
-		 * de utilizarse como el primer caracter de un identificador, es decir, del 
-		 * nombre de una variable
-		 * 
-		 *  Las variables que se declararan a nivel de la clase, no es necesario 
-		 *  asignarles ningun valor por defecto, porque se le asigna un valor por defecto
-		 *  automaticamente. Todo lo contrario ocurre con las variable locales. 
-		 *  Las varibles locales son aquellas que se declaran detro de un bloque, es decir,
-		 *  entre llaves de apertura y cierre {}.
-		 *  Aunque podemos declarar variables dentro de cualquier bloque {}, cuando nos
-		 *  referimos a variables locales significa dentro de un metodo */
-		
-		int total2 = 0 ;
-		
-		/* TIPOS DE DATO:
-		 * 
-		 * 1- Primitivos (el nombre es todo en minisculas)
-		 * 2- Objetos o Referencia (La primera letra del tipo es en mayusculas)
-		 * 
-		 * El tipo de dato que realmente existe es el tipo de dato primitivo, que esta
-		 * integrado en el propio lenguaje Java, porque los tipos Objetos o Referencia son 
-		 * un elvoltorio de los tipos primitivos, en resumen, que la gran mayoria de las
-		 * ocasiones debemos utilizar tipos de datos primitivos en lugar de su 
-		 * correspondiente tipo Objeto o Referencia. 
-		 * 
-		 * Los tipos de datos primitivos no tienen propiedades ni metodos, es decir, que 
-		 * si le agregamos el operador punto (.) no se va a desplegar ningun menu
-		 * contextual */
-		
-		Integer total3 = 24;
-		
-		
+		/* ¿Que es un Array? */
 	}
 }
 
